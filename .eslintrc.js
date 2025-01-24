d538c706c99c5aab707bc6cdf14135f1b595bbc6
jameskang <idelsoo@gmail.com>
2025-01-24 19:17:31 +0900

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,7 @@ module.exports = {
     "prefer-arrow-callback": "error",
     "quotes": ["error", "double", {"allowTemplateLiterals": true}],
     "camelcase": "off",
+    "max-len": ["error", {code: 120}],
   },
   overrides: [
     {
